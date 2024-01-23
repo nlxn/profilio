38940178c2b7ddcaf352a8084d74f7792df454bf
nlxn <157534850+nlxn@users.noreply.github.com>
2024-01-24 01:38:27 +0300

    Add files via upload

---
+++ data/config.js
@@ -0,0 +1,47 @@
+export const meta = {
+  title: 'Nelson Nathan - NLXN®',
+  description: 'Hey, I\'m Nelson. I\'m a web developer and graphic designer, providing success with every solution.'
+}
+
+export const hero = {
+  title: "Hey there! I'm Nelson Nathan.",
+  desc: "I'm a web developer and graphic designer, providing success with every solution. → NLXN® Studio"
+};
+
+export const projects = {
+  title: 'My Links',
+  desc: "Here are links of my portfolio and socials.",
+  projects: [
+    {
+      title: 'Behance',
+      description: 'Nelson Nathan-Portfolio',
+      link: 'https://behance.net/nlxn'
+    },
+    {
+      title: 'GitHub',
+      description: 'Coding stuff!',
+      github: 'nlxn'
+    },
+    {
+      title: 'Instagram',
+      description: 'Keeping it real!',
+      link: 'https://instagram.com/nlxn.xyz'
+    }
+  ]
+};
+
+export const stack = {
+  title: 'My Stack',
+  stack: [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'Illustrator',
+    'Photoshop'
+  ]
+};
+
+export const contact = {
+  title: 'Contact Me',
+  email: 'nelson@nlxn.xyz'
+};
